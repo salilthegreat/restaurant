bd8b651678493adea78e45e7389e8dc8b657f43b
salilthegreat <salilkumardas22@gmail.com>
2023-08-31 18:42:21 +0530

    Menu page design complete

--- src/data.ts
+++ src/data.ts
@@ -199,3 +199,39 @@ export const featuredProducts: Products = [
         ],
     },
 ];
+
+type Menu = {
+    id: number;
+    slug: string;
+    title: string;
+    desc?: string;
+    img?: string;
+    color: string;
+  }[];
+  
+  export const menu: Menu = [
+    {
+      id: 1,
+      slug: "pastas",
+      title: "Italian Pastas",
+      desc: "Savor the taste of perfection with our exquisite Italian handmade pasta menu.",
+      img: "/temp/m1.jpg",
+      color: "white",
+    },
+    {
+      id: 2,
+      slug: "burgers",
+      title: "Juicy Burgers",
+      desc: "Burger Bliss: Juicy patties, bold flavors, and gourmet toppings galore.",
+      img: "/temp/m3.jpg",
+      color: "black",
+    },
+    {
+      id: 3,
+      slug: "pizzas",
+      title: "Cheesy Pizzas",
+      desc: "Pizza Paradise: Irresistible slices, mouthwatering toppings, and cheesy perfection.",
+      img: "/temp/m4.jpg",
+      color: "white",
+    },
+  ];
