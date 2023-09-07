1f5094add5dffda80be733be99a0268dc9a6c478
salilthegreat <salilkumardas22@gmail.com>
2023-09-07 12:33:13 +0530

    cart page design complete

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,63 @@
+import Image from 'next/image'
 import React from 'react'
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
+    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh - 9rem)] flex flex-col text-red-500 lg:flex-row '>
+      {/* PRODUCT CONTAINER */}
+      <div className='h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:w-2/3 lg:h-full 2xl:w-1/2 lg:px-20 xl:px-40'>
+        {/* SINGLEITEM CONTAINER */}
+        <div className='flex items-center justify-between mb-4 '>
+          <Image src={"/temp/p1.png"} alt='' width={100} height={100}/>
+          <div className=''>
+            <h1 className='uppercase text-xl font-bold'>sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className='font-bold'>₹449</h2>
+          <span className='cursor-pointer'>X</span>
+        </div>
+        <div className='flex items-center justify-between mb-4 '>
+          <Image src={"/temp/p1.png"} alt='' width={100} height={100}/>
+          <div className=''>
+            <h1 className='uppercase text-xl font-bold'>sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className='font-bold'>₹449</h2>
+          <span className='cursor-pointer'>X</span>
+        </div>
+        <div className='flex items-center justify-between mb-4 '>
+          <Image src={"/temp/p1.png"} alt='' width={100} height={100}/>
+          <div className=''>
+            <h1 className='uppercase text-xl font-bold'>sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className='font-bold'>₹449</h2>
+          <span className='cursor-pointer'>X</span>
+        </div>
+        
+      </div>
+      {/* PAYMENT CONTAINER */}
+      <div className='h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:w-1/3 lg:h-full 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6' >
+        <div className='flex justify-between'>
+          <span className=''>Subtotal(3 item)</span>
+          <span className=''>₹1247</span>
+        </div>
+        <div className='flex justify-between'>
+          <span className=''>Service Cost</span>
+          <span className=''>₹0</span>
+        </div>
+        <div className='flex justify-between'>
+          <span className=''>Delivery Cost</span>
+          <span className='text-green-500'>FREE!</span>
+        </div>
+        <hr  className='my-2'/>
+        <div className='flex justify-between'>
+          <span className=''>Total(INC. VAT)</span>
+          <span className='font-bold'>₹1247</span>
+        </div>
+        <button className='bg-red-500 text-white p-3 rounded-md w-1/2 self-end'>CHECKOUT</button>
+      </div>
+    </div>
   )
 }
 
