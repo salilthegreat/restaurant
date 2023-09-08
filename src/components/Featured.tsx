a90029a27fca00f532c36e912343e538eb600cab
salilthegreat <salilkumardas22@gmail.com>
2023-09-08 16:44:20 +0530

    orderes page design complete

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -11,7 +11,7 @@ const Featured = () => {
         {featuredProducts.map((item) => (
           <div
             key={item.id}
-            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+            className="w-screen h-[80vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
           >
             {/* IMAGE CONTAINER */}
             {item.img && (
